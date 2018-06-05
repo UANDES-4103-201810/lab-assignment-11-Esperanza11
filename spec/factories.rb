323d8a8ec83f7a745bcf0d36f9f815cfa5559dda
Web <adaldunate@miuandes.cl>
2018-06-05 13:06:10 -0400

    5,6

---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :User do
+    username "Espe"
+    password 1 # integer value
+    email "lalala"
+  end
+# Add other factories in here
+  factory :Message do
+    user_id 1
+    date Date.today # integer value
+    email "lalala"
+  end
+end
